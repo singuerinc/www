30d0b8891108a8f3a16cd6ce13207f81d12ec87b
Nahuel Scotti <nahuel.scotti@gmail.com>
2014-10-15 18:53:27 +0200

    cache time

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public'), {maxAge: 432000000}));
+app.use(express.static(path.join(__dirname, 'public'), {maxAge: 432000000 * 2}));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
