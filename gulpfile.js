5032098610562743eedf0a72901eb38fac9d470e
Nahuel Scotti <nahuel.scotti@gmail.com>
2016-04-05 22:58:01 +0200

    update

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,17 @@ var uglify = require('gulp-uglify');
 var minifyCSS = require('gulp-minify-css');
 
 gulp.task('min-js', function() {
-   return gulp.src('src/all.js')
+   return gulp.src('./all.js')
     .pipe(uglify())
     .pipe(rename({ extname: '.min.js' }))
-    .pipe(gulp.dest('src/'));
+    .pipe(gulp.dest('./'));
 });
 
 gulp.task('min-css', function() {
-   return gulp.src('src/all.css')
+   return gulp.src('./all.css')
     .pipe(minifyCSS({keepBreaks:true}))
     .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('src/'));
+    .pipe(gulp.dest('./'));
 });
 
 gulp.task('default', ['min-css', 'min-js'], function() {
