34e3e8726498006f528fabfbdf12b62a642ee44f
singuerinc <nahuel.scotti@gmail.com>
2016-08-22 00:28:20 +0200

    add functional tests

--- test.js
+++
@@ -1,9 +0,0 @@
-casper.test.begin('Test', 1, function suite(test) {
-    casper.start('http://develop.www.singuerinc.com.surge.sh/', function() {
-        test.assertTitle("Nahuel Scotti - Portfolio", "homepage title is the one expected");
-    });
-
-    casper.run(function() {
-        test.done();
-    });
-});
