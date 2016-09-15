0cb081cfa0833b5f6b90e6314ade750ef017732f
singuerinc <nahuel.scotti@gmail.com>
2016-09-16 00:30:16 +0200

    sort related posts, add name to meta descriptions

--- _tests/index.js
+++ _tests/index.js
@@ -2,13 +2,13 @@ const testAll = function (test, client, title, clientAndTitle, agency, canonical
   test.assertHttpStatus(200);
   test.assertExists("link[rel='canonical'][href='https://www.singuerinc.com" + canonical + "']");
 
-  test.assertExists("meta[property='og:title'][content='" + clientAndTitle + "']");
+  test.assertExists("meta[property='og:title'][content='Nahuel Scotti - Portfolio: " + clientAndTitle + "']");
   test.assertExists("meta[property='og:description'][content='Developer. Currently working at NetEnt, Stockholm - Sweden.']");
   test.assertExists("meta[property='og:url'][content='https://www.singuerinc.com" + canonical + "']");
   test.assertExists("meta[property='og:image'][content='https://www.singuerinc.com/img/home/" + image + ".jpg']");
   test.assertExists("meta[property='og:image:width'][content='816']");
   test.assertExists("meta[property='og:image:height'][content='386']");
-  test.assertExists("meta[name='twitter:title'][content='" + clientAndTitle + "']");
+  test.assertExists("meta[name='twitter:title'][content='Nahuel Scotti - Portfolio: " + clientAndTitle + "']");
   test.assertExists("meta[name='twitter:description'][content='Developer. Currently working at NetEnt, Stockholm - Sweden.']");
   test.assertExists("meta[name='twitter:image'][content='https://www.singuerinc.com/img/home/" + image + ".jpg']");
 
@@ -32,7 +32,7 @@ const testAll = function (test, client, title, clientAndTitle, agency, canonical
   test.assertSelectorHasText(".share-title", "Share");
   test.assertExists(".share-post");
   test.assertElementCount(".share-post li", shareCount);
-  test.assertExists(".share-post li:nth-child(1) a[href='https://twitter.com/intent/tweet?text=" + encodeURI(clientAndTitle) + "&url=https://www.singuerinc.com" + canonical + "&via=singuerinc']");
+  test.assertExists(".share-post li:nth-child(1) a[href='https://twitter.com/intent/tweet?text=" + encodeURI("Nahuel Scotti - Portfolio: " + clientAndTitle) + "&url=https://www.singuerinc.com" + canonical + "&via=singuerinc']");
   test.assertExists(".share-post li:nth-child(2) a[href='https://www.facebook.com/sharer/sharer.php?u=https://www.singuerinc.com" + canonical + "']");
   test.assertExists(".share-post li:nth-child(3) a[href='https://plus.google.com/share?url=https://www.singuerinc.com" + canonical + "']");
 };
