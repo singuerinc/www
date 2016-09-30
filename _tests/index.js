207b34b090833e946759440a82ad87602318acaa
singuerinc <nahuel.scotti@gmail.com>
2016-09-30 22:28:06 +0200

    add social icons to sidebar

--- _tests/index.js
+++ _tests/index.js
@@ -1,4 +1,4 @@
-const TOTAL_TESTS = 1494;
+const TOTAL_TESTS = 1590;
 const projects = [
   {
     id: "zalando_ivy-park",
@@ -397,6 +397,11 @@ const testAll = function (test, project) {
   test.assertExists("body .sidebar-mobile ul li:nth-child(2) a[href='../about.html']");
   test.assertExists("body .sidebar nav ul:first-child li:nth-child(7) a[href='../sitemap.html']");
   test.assertExists("body .sidebar-mobile ul li:nth-child(7) a[href='../sitemap.html']");
+
+  test.assertElementCount("body .sidebar .social li a", 3);
+  test.assertExists("body .sidebar .social li a[href='https://twitter.com/intent/tweet?url=https://www.singuerinc.com&via=singuerinc']");
+  test.assertExists("body .sidebar .social li a[href='https://www.facebook.com/sharer/sharer.php?u=https://www.singuerinc.com']");
+  test.assertExists("body .sidebar .social li a[href='https://plus.google.com/share?url=https://www.singuerinc.com']");
 };
 
 casper.test.begin("Test", TOTAL_TESTS, function suite(test) {
@@ -437,7 +442,7 @@ casper.test.begin("Test", TOTAL_TESTS, function suite(test) {
     // sidebar - desktop - links
     test.assertExists("body .sidebar ul");
     // home, about, github, twitter, blog, medium
-    test.assertElementCount("body .sidebar nav ul:first-child li", 7);
+    test.assertElementCount("body .sidebar nav > ul:first-child > li", 8);
     test.assertSelectorHasText("body .sidebar nav ul:first-child li:nth-child(1) a", "Home");
     test.assertSelectorHasText("body .sidebar nav ul:first-child li:nth-child(2) a", "About");
     test.assertSelectorHasText("body .sidebar nav ul:first-child li:nth-child(3) a", "GitHub");
@@ -453,6 +458,11 @@ casper.test.begin("Test", TOTAL_TESTS, function suite(test) {
     test.assertExists("body .sidebar nav ul:first-child li:nth-child(5) a[href='https://blog.singuerinc.com']");
     test.assertExists("body .sidebar nav ul:first-child li:nth-child(6) a[href='https://medium.com/@singuerinc']");
     test.assertExists("body .sidebar nav ul:first-child li:nth-child(7) a[href='./sitemap.html']");
+
+    test.assertElementCount("body .sidebar .social li a", 3);
+    test.assertExists("body .sidebar .social li a[href='https://twitter.com/intent/tweet?url=https://www.singuerinc.com&via=singuerinc']");
+    test.assertExists("body .sidebar .social li a[href='https://www.facebook.com/sharer/sharer.php?u=https://www.singuerinc.com']");
+    test.assertExists("body .sidebar .social li a[href='https://plus.google.com/share?url=https://www.singuerinc.com']");
 
     // sidebar - mobile - links
     test.assertExists("body .sidebar-mobile ul");
@@ -554,6 +564,10 @@ casper.test.begin("Test", TOTAL_TESTS, function suite(test) {
       test.assertExists("body .sidebar-mobile ul li:nth-child(2) a[href='./about.html']");
       test.assertExists("body .sidebar nav ul:first-child li:nth-child(7) a[href='./sitemap.html']");
       test.assertExists("body .sidebar-mobile ul li:nth-child(7) a[href='./sitemap.html']");
+      test.assertElementCount("body .sidebar .social li a", 3);
+      test.assertExists("body .sidebar .social li a[href='https://twitter.com/intent/tweet?url=https://www.singuerinc.com&via=singuerinc']");
+      test.assertExists("body .sidebar .social li a[href='https://www.facebook.com/sharer/sharer.php?u=https://www.singuerinc.com']");
+      test.assertExists("body .sidebar .social li a[href='https://plus.google.com/share?url=https://www.singuerinc.com']");
     })
     .thenOpen("http://jekyll:4000/sitemap.html", function () {
       test.assertTextExists("ga('create', 'UA-881783-8', {'cookieDomain': 'none'});");
@@ -568,6 +582,10 @@ casper.test.begin("Test", TOTAL_TESTS, function suite(test) {
       test.assertExists("body .sidebar-mobile ul li:nth-child(2) a[href='./about.html']");
       test.assertExists("body .sidebar nav ul:first-child li:nth-child(7) a[href='./sitemap.html']");
       test.assertExists("body .sidebar-mobile ul li:nth-child(7) a[href='./sitemap.html']");
+      test.assertElementCount("body .sidebar .social li a", 3);
+      test.assertExists("body .sidebar .social li a[href='https://twitter.com/intent/tweet?url=https://www.singuerinc.com&via=singuerinc']");
+      test.assertExists("body .sidebar .social li a[href='https://www.facebook.com/sharer/sharer.php?u=https://www.singuerinc.com']");
+      test.assertExists("body .sidebar .social li a[href='https://plus.google.com/share?url=https://www.singuerinc.com']");
     })
     .thenOpen("http://jekyll:4000/b-reel/zalando-ivy-park.html", function () {
       testAll(test, projects[0]);
