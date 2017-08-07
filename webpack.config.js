5e66dcce6338edf394314cb21006c0317924c39b
singuerinc <nahuel.scotti@gmail.com>
2017-08-07 23:11:20 +0200

    feat: webpack production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,3 @@
-/* global require, module, __dirname */
-module.exports = {
-  entry: ['./_src/index.ts'],
-  devtool: 'cheap-eval-source-map',
-  output: {
-    path: `${__dirname}/assets`,
-    filename: 'bundle.js',
-    publicPath: '/assets/',
-    library: 'app',
-    libraryTarget: 'umd',
-  },
-  plugins: [],
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
-    modules: [
-      "node_modules"
-    ]
-  },
-  module: {
-    rules: [
-      { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
-    ],
-  },
+module.exports = function(env) {
+  return require(`./webpack.${env}.js`)
 };
