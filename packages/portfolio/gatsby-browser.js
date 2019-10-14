6a7c1b9d192b0a4840d58e3da8fda1881f2a6dc1
singuerinc <nahuel.scotti@gmail.com>
2019-10-14 18:07:40 +0200

    feat: netlify ready

--- packages/portfolio/gatsby-browser.js
+++ packages/portfolio/gatsby-browser.js
@@ -1,22 +1,3 @@
 import "./src/styles/global.css"
-import { init, Portfolio } from "app"
 
-export const onInitialClientRender = () => {
-  const posts = []
-  {
-    edges.map(
-      ({
-        node: {
-          frontmatter: { client, title, path, image },
-        },
-      }) =>
-        `window.posts.push({id: "${dashify({
-          client,
-          title,
-        })}", image: "${image}"});`
-    )
-  }
-
-  console.log(posts)
-  init(document, new Portfolio(), [])
-}
+export const onInitialClientRender = () => {}
