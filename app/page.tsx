c29bb9b801c77fe5688a597cf92f3f95e39ea45c
Nahuel Scotti <nahuel.scotti@gmail.com>
2023-05-15 16:24:12 +0200

    feat: working #wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { getAllPosts } from "@/lib/post";
 import {
   dashify,
+  getAbsoluteProjectImage,
+  getAbsoluteProjectUrl,
   getProjectImage,
   getProjectUrl,
   normalizeTitle,
@@ -10,11 +12,10 @@ export default async function Page() {
   const posts = getAllPosts();
 
   return (
-    <ul className="grid grid-cols-1 mx-auto md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 posts">
-      <div className="flex flex-col justify-around p-12">
-        <h1 className="font-normal">singuerinc</h1>
-        <h2 className="font-normal">frontend developer</h2>
-        <p>my work</p>
+    <ul className="grid grid-cols-1 mx-auto md:grid-cols-2 xl:grid-cols-3 3xl:grid-cols-4">
+      <div className="flex flex-col justify-center p-12">
+        <h1 className="text-4xl font-normal">singuerinc | dev</h1>
+        <p>nahuel scotti • my work</p>
       </div>
       {posts.map((post, key) => {
         const id = dashify(post);
@@ -23,7 +24,7 @@ export default async function Page() {
           <li
             key={key}
             id={id}
-            className="post"
+            className="relative w-full aspect-[16/8] bg-center bg-no-repeat bg-cover"
             itemScope
             itemType="http://schema.org/WebSite"
             style={{ backgroundImage: `url(${getProjectImage(post.image)})` }}
@@ -34,12 +35,22 @@ export default async function Page() {
             {post.awards?.map((a, idx) => (
               <meta key={idx} itemProp="award" content={a} />
             ))}
-            <meta itemProp="image" content={getProjectImage(post.image)} />
-            <meta itemProp="url" content={getProjectUrl(post.slug)} />
-            <a href={post.slug} target="_self" className="w-link animated">
-              {/* <div className={`post-image animated ${id}`}></div> */}
-              <h3 className="w-title">{title}</h3>
-              <div className="w-tags">{post.tech.join(" · ")}</div>
+            <meta
+              itemProp="image"
+              content={getAbsoluteProjectImage(post.image)}
+            />
+            <meta itemProp="url" content={getAbsoluteProjectUrl(post.slug)} />
+            <a
+              href={post.slug}
+              target="_self"
+              className="absolute flex flex-col items-end w-full h-full transition-all duration-1000 delay-75 w-link md:p-12 group md:justify-end md:items-start animated hover:shadow-2xl hover:z-50 hover:bg-gradient-to-r from-black to-black/80"
+            >
+              <h3 className="hidden w-full pb-2 mb-2 text-lg font-normal text-white transition-all duration-1000 delay-200 pointer-events-none md:opacity-0 md:flex md:group-hover:opacity-100 md:border-b border-b-white/20">
+                {title}
+              </h3>
+              <div className="hidden w-full pt-2 text-xs font-medium text-white transition-all duration-1000 delay-200 pointer-events-none md:opacity-0 md:flex md:group-hover:opacity-100 text-white/70">
+                {post.tech.join(" · ")}
+              </div>
             </a>
           </li>
         );
