d68f1b73113f8c6af8004045c27ff0c31d2aea51
Nahuel Scotti <nahuel.scotti@gmail.com>
2023-05-16 09:07:11 +0200

    feat: working #wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { About } from "@/components/About";
 import { CurrentStack } from "@/components/CurrentStack";
+import { ProjectLink } from "@/components/ProjectLink";
 import { Title } from "@/components/Title";
 import { getAllPosts } from "@/lib/post";
 import {
@@ -7,10 +8,8 @@ import {
   getAbsoluteProjectImage,
   getAbsoluteProjectUrl,
   getProjectImage,
-  getProjectUrl,
   normalizeTitle,
 } from "@/lib/utils";
-import Link from "next/link";
 
 export default async function Page() {
   const posts = getAllPosts();
@@ -42,17 +41,7 @@ export default async function Page() {
                 content={getAbsoluteProjectImage(post.image)}
               />
               <meta itemProp="url" content={getAbsoluteProjectUrl(post.slug)} />
-              <Link
-                href={getProjectUrl(post.slug)}
-                className="absolute flex flex-col items-end w-full h-full transition-all w-link md:p-12 group md:justify-end md:items-start md:hover:shadow-2xl md:hover:z-50 md:bg-gradient-to-t md:hover:from-black md:hover:to-black/70"
-              >
-                <h3 className="hidden w-full pb-2 mb-2 text-lg font-normal text-white transition-all pointer-events-none md:opacity-0 md:flex md:group-hover:opacity-100 md:border-b border-b-white/20">
-                  {title}
-                </h3>
-                <div className="hidden w-full pt-2 font-mono text-xs font-medium text-white transition-all pointer-events-none md:opacity-0 md:flex md:group-hover:opacity-100 text-white/70">
-                  {post.tech.join(" · ")}
-                </div>
-              </Link>
+              <ProjectLink post={post} />
             </li>
             {idx === 7 && <About />}
             {idx === 11 && <CurrentStack />}
