2db428bce5a7b8c661074d5cd70e9bdfc123f26e
Nahuel Scotti <nahuel.scotti@gmail.com>
2023-05-19 23:22:53 +0200

    feat: smooth scroll

--- components/ScrollObserver.tsx
+++ components/ScrollObserver.tsx
@@ -1,11 +1,21 @@
 "use client";
 
+import { useMotionValueEvent, useSpring } from "framer-motion";
 import { useCallback, useEffect } from "react";
 
 export function ScrollObserver() {
-  const onScroll = useCallback((e: WheelEvent) => {
-    document.body.scrollLeft += e.deltaY;
-  }, []);
+  const x = useSpring(0, { stiffness: 250 });
+
+  const onScroll = useCallback(
+    (e: WheelEvent) => {
+      x.set(document.body.scrollLeft + e.deltaY);
+    },
+    [x]
+  );
+
+  useMotionValueEvent(x, "change", (latest) => {
+    document.body.scrollLeft = latest;
+  });
 
   useEffect(() => {
     window.addEventListener("wheel", onScroll);
