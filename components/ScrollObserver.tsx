9f251a3625c285a5f923418351fe36a191046d7a
Nahuel Scotti <nahuel.scotti@gmail.com>
2023-09-25 14:01:10 +0200

    fix: horizontal scroll

--- components/ScrollObserver.tsx
+++ components/ScrollObserver.tsx
@@ -1,25 +1,16 @@
 "use client";
 
-import { useMotionValueEvent, useSpring } from "framer-motion";
 import { useCallback, useEffect } from "react";
 
 export function ScrollObserver() {
-  const x = useSpring(0, { stiffness: 100 });
-
-  const onScroll = useCallback(
-    (e: WheelEvent) => {
-      x.set(document.body.scrollLeft + e.deltaY);
-    },
-    [x]
-  );
-
-  useMotionValueEvent(x, "change", (latest) => {
-    document.body.scrollLeft = latest;
-  });
+  const onScroll = useCallback((e: WheelEvent) => {
+    document.body.scrollLeft += e.deltaY;
+  }, []);
 
   useEffect(() => {
     window.addEventListener("wheel", onScroll);
     return () => window.removeEventListener("wheel", onScroll);
   });
+
   return null;
 }
